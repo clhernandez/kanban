6af030e8bef0ce0aa83453598e553422edc45ae4
Claudio Hernandez <claudio.hernandez.p@usach.cl>
2015-11-26 12:39:15 -0400

    Change Task Color, implement add and remove task

--- js/core.js
+++ js/core.js
@@ -1,11 +1,12 @@
-(function() {
-	//Global Var
-	var grid = document.getElementById("laneHolder");
-	var flagLaneDAD = true;
-	var addLaneLink = document.getElementById("addLane");
-	var toggleDADLink = document.getElementById("toggleDAD");
+//Global Var
+var grid = document.getElementById("laneHolder");
+var flagLaneDAD = true;
+var addLaneLink = document.getElementById("addLane");
+var toggleDADLink = document.getElementById("toggleDAD");
+var dev;//only for dev purposes.
 
-	//Initial Events
+(function() {
+	//Initial Events that execute only when dom is ready.
 	addLaneLink.addEventListener("click", addLane, false);
 	toggleDADLink.addEventListener("click", toggleLaneDADCard, false);
 	appendDAD(document.getElementById("init"));//only in development
@@ -21,78 +22,82 @@
 		appendDADfa(trs[i]);	
 	};
 
-	function createNewLane(){
-		var newLane = document.createElement("div");
-		var newTitle = document.createElement("div");
-		var newContent = document.createElement("div");
-		newLane.className = "mdl-card";
-		newLane.draggable = "true";
-
-		newTitle.innerHTML = "New Lane";
-		newTitle.contentEditable = "true";
-		newLane.appendChild(newTitle);
-		newContent.id = "1";
-		newLane.appendChild(newContent);
-		return newLane;
-	}
-
-	function createNewTaskLane(){
-		var lane = document.createElement("div");
-		lane.id = CryptoJS.MD5(getRandom());
-		lane.className = "col-md-3-5 col-xs-12 lane"
-		//lane.draggable = "true";
-			var laneTitle = document.createElement("div");
-			laneTitle.className = "lane-title";
-				laneTitle.contentEditable = "true";
-				laneTitle.innerHTML = "New Lane";
-		lane.appendChild(laneTitle);
-			var laneTask = document.createElement("div");
-			laneTask.className = "lane-tasks";
-				var task = document.createElement("div")
-				task.className = "task clearfix";
-					var taskTitle = document.createElement("div");
-					taskTitle.className = "task-title pull-left";
-					taskTitle.contentEditable = "true";
-					taskTitle.innerHTML = "New Task";
-				task.appendChild(taskTitle);
-					var taskOpts = document.createElement("div");
-					taskOpts.className = "task-opts pull-left";
-						var linkTaskOpts = document.createElement("span");
-						linkTaskOpts.className = "glyphicon glyphicon-edit";
-						linkTaskOpts.setAttribute("onClick", "toogleTaskEdit(this);");
-					taskOpts.appendChild(linkTaskOpts);
-				task.appendChild(taskOpts);
-					var taskNotes = document.createElement("div");
-					taskNotes.className = "task-notes pull-left";
-					taskNotes.style.display = "none";
-						var taskDate = document.createElement("div");
-						taskDate.className = "task-date";
-						taskDate.contentEditable = "true";
-						taskDate.innerHTML = moment().format("DD/MM/YYYY");
-					taskNotes.appendChild(taskDate);
-						var taskText = document.createElement("div");
-						taskText.className = "task-text";
-						taskText.contentEditable = "true";
-						taskText.innerHTML = "Some text related to the task.";
-					taskNotes.appendChild(taskText);
-				task.appendChild(taskNotes);
-			laneTask.appendChild(task);
-		lane.appendChild(laneTask);
-		return lane;
-	}
+	flagLaneDAD = (flagLaneDAD)? false : true;
+	toggleDADLink.parentElement.className = (flagLaneDAD)? "active" : "";
+})();
 
 	function addLane(){
 		var taskLanes = document.getElementsByClassName("lane");
 		var countTaskLanes = taskLanes.length;
 
 		if(countTaskLanes < 4 ){
-			var newLane = createNewTaskLane();
+			var newLane = createNewLane();
 			grid.appendChild(newLane);
 			//appendDAD(newLane);	
 		}
 
 	}
 
+	function createNewLane(){
+		var lane = document.createElement("div");
+		lane.id = CryptoJS.MD5(getRandom());
+		lane.className = "col-md-3-5 col-xs-12 lane"
+		//lane.draggable = "true";
+			var laneTitle = document.createElement("div");
+			laneTitle.className = "lane-title clearfix";
+				var title = document.createElement("span");
+				title.className = "pull-left";
+				title.contentEditable = "true";
+				title.innerHTML = "New Lane";
+			laneTitle.appendChild(title);
+				var btnAddtask = document.createElement("span");
+				btnAddtask.className = "glyphicon glyphicon-plus pull-right";
+				btnAddtask.setAttribute("onClick", "addTaskToLane(this);");
+			laneTitle.appendChild(btnAddtask);
+		lane.appendChild(laneTitle);
+			var laneTask = document.createElement("div");
+			laneTask.className = "lane-tasks";
+			laneTask.appendChild(createNewTask());
+		lane.appendChild(laneTask);
+		return lane;
+	}
+
+	function createNewTask(){
+		var task = document.createElement("div")
+		task.className = "task clearfix";
+			var taskTitle = document.createElement("div");
+			taskTitle.className = "task-title pull-left";
+			taskTitle.contentEditable = "true";
+			taskTitle.innerHTML = "New Task";
+		task.appendChild(taskTitle);
+			var taskOpts = document.createElement("div");
+			taskOpts.className = "task-opts pull-left";
+				var linkTaskEdit = document.createElement("span");
+				linkTaskEdit.className = "glyphicon glyphicon-edit";
+				linkTaskEdit.setAttribute("onClick", "toogleTaskEdit(this);");
+			taskOpts.appendChild(linkTaskEdit);
+			taskOpts.appendChild(document.createTextNode(" "));
+				var linkTaskRemove = document.createElement("span");
+				linkTaskRemove.className = "glyphicon glyphicon-remove"
+				linkTaskRemove.setAttribute("onClick", "removeTask(this);");
+			taskOpts.appendChild(linkTaskRemove);
+		task.appendChild(taskOpts);
+			var taskNotes = document.createElement("div");
+			taskNotes.className = "task-notes pull-left";
+			taskNotes.style.display = "none";
+				var taskDate = document.createElement("div");
+				taskDate.className = "task-date";
+				taskDate.contentEditable = "true";
+				taskDate.innerHTML = moment().format("DD/MM/YYYY");
+			taskNotes.appendChild(taskDate);
+				var taskText = document.createElement("div");
+				taskText.className = "task-text";
+				taskText.contentEditable = "true";
+				taskText.innerHTML = "Some text related to the task.";
+			taskNotes.appendChild(taskText);
+		task.appendChild(taskNotes);
+		return task;
+	}
 	
 	function toggleLaneDADCard(){
 		cells = document.querySelectorAll('.mdl-card');
@@ -105,33 +110,38 @@
 				cells[i].draggable = true;
 			}
 		}
-		flagLaneDAD = (flagLaneDAD)? false : true;
-		toggleDADLink.parentElement.className = (flagLaneDAD)? "active" : "";
 	}
 
-		
-})();
+	function addTaskToLane(el){
+		var laneTasks = el.parentElement.parentElement.getElementsByClassName("lane-tasks")[0];
+		if(laneTasks!=null)
+			laneTasks.appendChild(createNewTask());
+	}
 
-function getRandom(){
-	var arr = []
-	while(arr.length < 8){
-		var randomnumber=Math.ceil(Math.random()*100)
-		var found=false;
-		for(var i=0;i<arr.length;i++){
-			if(arr[i]==randomnumber){found=true;break}
+	function removeTask(el){
+		el.parentElement.parentElement.remove();
+	}
+
+	function getRandom(){
+		var arr = []
+		while(arr.length < 8){
+			var randomnumber=Math.ceil(Math.random()*100)
+			var found=false;
+			for(var i=0;i<arr.length;i++){
+				if(arr[i]==randomnumber){found=true;break}
+			}
+			if(!found)arr[arr.length]=randomnumber;
 		}
-		if(!found)arr[arr.length]=randomnumber;
+		return arr.toString();
 	}
-	return arr.toString();
-}
 
 
-function toogleTaskEdit(el){
-	if(el.parentElement.parentElement.children[2]!=null){
-		if(el.parentElement.parentElement.children[2].style.display != "block"){
-			el.parentElement.parentElement.children[2].style.display = "block";
-		}else{
-			el.parentElement.parentElement.children[2].style.display = "none";
+	function toogleTaskEdit(el){
+		if(el.parentElement.parentElement.children[2]!=null){
+			if(el.parentElement.parentElement.children[2].style.display != "block"){
+				el.parentElement.parentElement.children[2].style.display = "block";
+			}else{
+				el.parentElement.parentElement.children[2].style.display = "none";
+			}
 		}
 	}
-}
